0d7a754f4f249ed70d717a283d14ba6700b0def4
EPMufasa <49794549+EPMufasa@users.noreply.github.com>
2019-04-19 15:55:08 -0300

    Add files via upload

---
+++ Juego.java
@@ -0,0 +1,61 @@
+package juego;
+
+import java.util.Scanner;
+
+public class Juego {
+	
+	public static void main(String[] args) {
+		Mazo m= new Mazo();
+		ManoDeJodete manojugador= new ManoDeJodete();
+		Carta pozo;
+		Scanner scan= new Scanner(System.in);
+		for(int i=0; i<=4; i++) { //le doy 5 cartas al jugador
+			manojugador.recibirCarta(m.mazo[0]);
+			m.mazo[0]=null; //las quito del mazo
+			m.acomodarmazo();// las acomodo
+		  }
+		manojugador.mostrar();
+		
+		pozo= new Carta(m.mazo[0].palo, m.mazo[0].numero);
+		m.mazo[0]=null;//la quito del mazo
+
+		m.acomodarmazo();//acomodo nuevamente el mazo
+		System.out.println("Carta en la mesa: " + "PALO: " + pozo.palo + " NUMERO: " + pozo.numero);
+		System.out.println(manojugador.cantidadDeCartas());
+		while(manojugador.cantidadDeCartas()!=0) {
+			int control= manojugador.cantidadDeCartas(); //para saber la cantidad inicial
+			boolean nohay=true;//para saber si hay cartas que podes tirar 
+			
+			for(int i= 0; i<control; i++) { 
+				if(manojugador.puedejugarla(manojugador.vacio[i], pozo)) { //verifico si puede jugar carta o no
+					System.out.println("*Podes jugar: " + i );
+					nohay=false;
+				}
+				else {
+					if(i==control-1 && nohay==true) {
+						manojugador.recibirCarta(m.mazo[0]);//le doy una carta al player
+						m.mazo[0]=null;//la elimino del mazo;
+						m.acomodarmazo();//acomodo nuevamente el mazo
+						System.out.println("---No podes jugar nada, recibis una carta :v");
+					}
+				}
+				if(i==control-1 && nohay==false) {
+					System.out.println("---Ingrese la carta que desea jugar");
+					int numero= scan.nextInt();
+					pozo = manojugador.jugarCarta(numero);
+					manojugador.acomodar01();
+				}
+
+			}
+		control= manojugador.cantidadDeCartas(); //para saber la cantidad inicial
+		nohay=true;//para saber si hay cartas que podes tirar 	
+		System.out.println("Actualizacion de estado");
+		System.out.println("Carta en la mesa: "+ "Palo: " + pozo.palo + " Numero: " + pozo.numero);
+		manojugador.mostrar();
+		System.out.println(m.cantidadDeCartasMazo() + "Cantidad mazo");
+		
+		}
+	}
+	
+}
+
